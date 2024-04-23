b108a70e3b1a0b995d97a8451498dd29358e31dd
dlekgus <97431420+dlekgus@users.noreply.github.com>
2024-04-23 18:47:14 +0900

    앱바, 그리드뷰 등등

---
+++ lib/src/widgets/app_bar.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../pages/p_regist_cloth.dart';
+import 'image_data.dart';
+
+//기본 앱바
+class BaseAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final bool showAddButton;
+
+  BaseAppBar({this.showAddButton = true});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      toolbarHeight: 105,
+      leadingWidth: 500,
+      leading: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 24, right: 24, top: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Image.asset(
+                  IconPath.topLogo,
+                  width: 170,
+                ),
+                if (showAddButton)
+                  GestureDetector(
+                    onTap: () {
+                      // 추가 아이콘 클릭 시 동작
+                      Get.to(AddPage()); // Add 페이지로 이동
+                    },
+                    child: Image.asset(
+                      IconPath.add,
+                      width: 30,
+                    ), // add 이미지
+                  ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(105); // AppBar의 높이 지정
+}
+
+class BaseAppBarController extends GetxController {
+  RxBool showAddButton = true.obs;
+
+  void toggleAddButtonVisibility() {
+    showAddButton.value = !showAddButton.value;
+  }
+}
+
+//양쪽 버튼, 가운데 글씨 앱바
+class TwoSelectAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const TwoSelectAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      toolbarHeight: 105,
+      leadingWidth: 500,
+      leading: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 24, right: 24, top: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                    onPressed: () {
+                      print("뒤로가기버튼클릭");
+                    },
+                    icon: Image.asset(
+                      IconPath.goBack,
+                      width: 24,
+                    )),
+                const Text(
+                  "의류 등록",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                ),
+                FilledButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "등록",
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => Size.fromHeight(105);
+}
