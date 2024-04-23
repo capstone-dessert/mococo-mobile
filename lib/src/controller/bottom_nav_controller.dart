6ee29457a9867c5b5ef1dc5093145150e7b18d4c
dlekgus <97431420+dlekgus@users.noreply.github.com>
2024-04-23 20:05:21 +0900

    머지

--- lib/src/controller/bottom_nav_controller.dart
+++ lib/src/controller/bottom_nav_controller.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../components/image_data.dart';
+import '../widgets/image_data.dart';
 
 class CustomBottomNavigationBar extends StatelessWidget {
   @override
@@ -9,13 +9,18 @@ class CustomBottomNavigationBar extends StatelessWidget {
       showUnselectedLabels: false,
       items: [
         BottomNavigationBarItem(
-          icon: Image.asset(IconPath.closetOff, width: 55, height: 55,),
+          icon: Image.asset(
+            IconPath.closetOff,
+            width: 55,
+            height: 55,
+          ),
           activeIcon: Image.asset(IconPath.closetOn, width: 55, height: 55),
           label: 'closet',
         ),
         BottomNavigationBarItem(
           icon: Image.asset(IconPath.codiRecommendOff, width: 55, height: 55),
-          activeIcon: Image.asset(IconPath.codiRecommendOn, width: 55, height: 55),
+          activeIcon:
+              Image.asset(IconPath.codiRecommendOn, width: 55, height: 55),
           label: 'codiRecommend',
         ),
         BottomNavigationBarItem(
