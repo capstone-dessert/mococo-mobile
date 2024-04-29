0470accd94fa0294bcf02f835fd5eb7e19d97bd2
dlekgus <97431420+dlekgus@users.noreply.github.com>
2024-04-29 13:16:42 +0900

    태그 그리드

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -11,54 +11,73 @@ class App extends GetView<BottomNavController> {
   @override
   Widget build(BuildContext context) {
     return NavigatorPopHandler(
-      onPop: () => controller.pop(),
-      child: Obx(
-        () => Scaffold(
-          appBar: AppBar(toolbarHeight: 35,),
-          body: IndexedStack(
-            index: controller.pageIndex.value,
-            children: [
-              const Closet(),
-              // 탭바 유지한 채 페이지 이동
-              Navigator(
-                key: controller.navigatorKey,
-                onGenerateRoute: (routeSettings) {
-                  return MaterialPageRoute(builder: (context) => const CodiRecommend());
-                },
-              ) ,
-              Container(child: const Center(child: Text('CODY_RECORD')),),
-            ],
-          ),
-          bottomNavigationBar: SizedBox(
-            height: 83,
-            child: BottomNavigationBar(
-              type: BottomNavigationBarType.fixed,
-              currentIndex: controller.pageIndex.value,
-              onTap: controller.changeBottomNav,
-              selectedItemColor: Colors.black,
-              selectedLabelStyle: const TextStyle(fontSize: 11),
-              unselectedLabelStyle: const TextStyle(fontSize: 11),
-              items: [
-                BottomNavigationBarItem(
-                  icon: SizedBox(height: 27, width: 27, child: Image.asset(IconPath.closetOff)),
-                  activeIcon: SizedBox(height:  27, width: 27, child: Image.asset(IconPath.closetOn)),
-                  label: '옷장'
+        onPop: () => controller.pop(),
+        child: Obx(
+          () => Scaffold(
+            appBar: AppBar(
+              toolbarHeight: 35,
+            ),
+            body: IndexedStack(
+              index: controller.pageIndex.value,
+              children: [
+                const Closet(),
+                // 탭바 유지한 채 페이지 이동
+                Navigator(
+                  key: controller.navigatorKey,
+                  onGenerateRoute: (routeSettings) {
+                    return MaterialPageRoute(
+                        builder: (context) => const CodiRecommend());
+                  },
                 ),
-                BottomNavigationBarItem(
-                  icon: SizedBox(height: 25.04, width: 24, child: Image.asset(IconPath.codiRecommendOff)),
-                  activeIcon: SizedBox(height: 25.04, width: 24, child: Image.asset(IconPath.codiRecommendOn)),
-                  label: '코디 추천'
+                Container(
+                  child: const Center(child: Text('CODY_RECORD')),
                 ),
-                BottomNavigationBarItem(
-                  icon: SizedBox(height: 26, width: 27, child:Image.asset(IconPath.codiRecordOff)),
-                  activeIcon: SizedBox(height: 26, width: 27, child: Image.asset(IconPath.codiRecordOn)),
-                  label: '코디 기록'
-                )
               ],
             ),
+            bottomNavigationBar: SizedBox(
+              height: 83,
+              child: BottomNavigationBar(
+                type: BottomNavigationBarType.fixed,
+                currentIndex: controller.pageIndex.value,
+                onTap: controller.changeBottomNav,
+                selectedItemColor: Colors.black,
+                selectedLabelStyle: const TextStyle(fontSize: 11),
+                unselectedLabelStyle: const TextStyle(fontSize: 11),
+                items: [
+                  BottomNavigationBarItem(
+                      icon: SizedBox(
+                          height: 27,
+                          width: 27,
+                          child: Image.asset(IconPath.closetOff)),
+                      activeIcon: SizedBox(
+                          height: 27,
+                          width: 27,
+                          child: Image.asset(IconPath.closetOn)),
+                      label: '옷장'),
+                  BottomNavigationBarItem(
+                      icon: SizedBox(
+                          height: 25.04,
+                          width: 24,
+                          child: Image.asset(IconPath.codiRecommendOff)),
+                      activeIcon: SizedBox(
+                          height: 25.04,
+                          width: 24,
+                          child: Image.asset(IconPath.codiRecommendOn)),
+                      label: '코디 추천'),
+                  BottomNavigationBarItem(
+                      icon: SizedBox(
+                          height: 26,
+                          width: 27,
+                          child: Image.asset(IconPath.codiRecordOff)),
+                      activeIcon: SizedBox(
+                          height: 26,
+                          width: 27,
+                          child: Image.asset(IconPath.codiRecordOn)),
+                      label: '코디 기록')
+                ],
+              ),
+            ),
           ),
-        ),
-      )
-    );
+        ));
   }
 }
--- lib/src/widgets/bottom_nav_controller .dart
+++ lib/src/widgets/bottom_nav_controller .dart
@@ -27,7 +27,28 @@ class CustomBottomNavigationBar extends StatelessWidget {
           icon: Image.asset(IconPath.codiRecordOff, width: 55, height: 55),
           activeIcon: Image.asset(IconPath.codiRecordOn, width: 55, height: 55),
           label: 'codiRecord',
-        ),      ],
+        ),
+      ],
     );
+  }
+}
+
+class SearchBottomNavBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: [
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              IconPath.closetOff,
+              width: 55,
+              height: 55,
+            ),
+            activeIcon: Image.asset(IconPath.closetOn, width: 55, height: 55),
+            label: 'closet',
+          ),
+        ]);
   }
 }
