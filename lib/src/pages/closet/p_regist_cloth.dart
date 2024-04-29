52f534c71afd7171f1bfc6c3aca747d93eebdeeb
dlekgus <97431420+dlekgus@users.noreply.github.com>
2024-04-30 04:48:57 +0900

    색상태그 그리드 추가

--- lib/src/pages/closet/p_regist_cloth.dart
+++ lib/src/pages/closet/p_regist_cloth.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../widgets/app_bar.dart';
+import '../../widgets/tag_list.dart';
 
 class AddPage extends StatelessWidget {
   const AddPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    Get.find<BaseAppBarController>().toggleAddButtonVisibility();
-
-    return Scaffold(
-      appBar: const BaseAppBar(showAddButton: false),
-      body: Container(),
+    return const Scaffold(
+      appBar: BaseAppBar(),
+      body: Column(
+        children: [
+          Flexible(
+            flex: 1, // 첫 번째 페이지의 비율
+            child: CatTagviewPage(),
+          ),
+          Flexible(
+            flex: 1, // 두 번째 페이지의 비율
+            child: ColorTagviewPage(),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/src/widgets/tag_list.dart
+++ lib/src/widgets/tag_list.dart
@@ -1,38 +1,157 @@
 import 'package:flutter/material.dart';
 
-class TagviewPage extends StatefulWidget {
-  const TagviewPage({super.key});
+class CatTagviewPage extends StatefulWidget {
+  const CatTagviewPage({super.key});
 
   @override
-  TagviewPageState createState() => TagviewPageState();
+  CatTagviewPageState createState() => CatTagviewPageState();
 }
 
-//목록 그리드로 보기
-class TagviewPageState extends State<TagviewPage> {
+//카테고리 태그 선택 그리드
+class CatTagviewPageState extends State<CatTagviewPage> {
+  List<String> buttonTexts = [
+    "# 상의",
+    "# 하의",
+    "# 아우터",
+    "# 원피스",
+    "# 신발",
+    "# 모자",
+    "# 가방",
+    "# 악세사리",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: GridView.builder(
-          itemCount: 14, //item 개수
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3, //1 개의 행에 보여줄 item 개수
-            childAspectRatio: 5 / 2, //item 의 가로 1, 세로 2 의 비율
-            mainAxisSpacing: 8, //수평 Padding
-            crossAxisSpacing: 15, //수직 Padding
-          ),
-          itemBuilder: (BuildContext context, int index) {
-            //item 의 반목문 항목 형성
-            return Container(
-              color: Colors.white,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text('#$index'),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(left: 6, top: 15, bottom: 10),
+                child: Row(
+                  children: [
+                    Text(
+                      '카테고리',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(width: 5),
+                    Text(
+                      '(필수)',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black87,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            );
-          },
+              GridView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(), // 그리드 뷰의 스크롤 제어
+                itemCount: buttonTexts.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 5 / 2,
+                  mainAxisSpacing: 8,
+                  crossAxisSpacing: 15,
+                ),
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    color: Colors.black87,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(buttonTexts[index]),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+//색상 태그 선택 그리드
+class ColorTagviewPageState extends State<ColorTagviewPage> {
+  List<String> buttonTexts = [
+    "# 화이트",
+    "# 블랙",
+    "# 그레이",
+    "# 차콜",
+    "# 빨강",
+    "# 핑크",
+    "# 연핑크",
+    "# 네이비",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(left: 6, top: 15, bottom: 10),
+                child: Row(
+                  children: [
+                    Text(
+                      '색상',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(width: 5),
+                    Text(
+                      '(필수)',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black87,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              GridView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: buttonTexts.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 5 / 2,
+                  mainAxisSpacing: 8,
+                  crossAxisSpacing: 15,
+                ),
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    color: Colors.black87,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(buttonTexts[index]),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ColorTagviewPage extends StatefulWidget {
+  const ColorTagviewPage({super.key});
+
+  @override
+  ColorTagviewPageState createState() => ColorTagviewPageState();
 }
