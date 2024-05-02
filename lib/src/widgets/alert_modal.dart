3571d8220e78cee6f46b53e91c357612cf4e93e1
kimsojeong <rtw0202@naver.com>
2024-05-02 11:12:59 +0900

    삭제 확인 modal 디자인 수정

--- lib/src/widgets/alert_modal.dart
+++ lib/src/widgets/alert_modal.dart
@@ -1,30 +1,52 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class AlertModal {
   static void show(BuildContext context) {
     showCupertinoModalPopup<void>(
       context: context,
-      builder: (BuildContext context) =>
-          CupertinoAlertDialog(
-            title: const Text('의류를 삭제하시겠습니까?'),
-            // content: const Text('Proceed with destructive action?'),
-            actions: <CupertinoDialogAction>[
-              CupertinoDialogAction(
-                isDefaultAction: true,
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text('취소'),
-              ),
-              CupertinoDialogAction(
-                isDestructiveAction: true,
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text('삭제'),
-              ),
-            ],
+      builder: (BuildContext context) => CupertinoAlertDialog(
+        content: const Text(
+          '의류를 삭제하시겠습니까?',
+          style: TextStyle(
+            fontSize: 17,
           ),
+        ),
+        actions: <Widget>[
+          CupertinoDialogAction(
+            child: const Text(
+              "취소",
+              style: TextStyle(
+                fontSize: 17,
+                color: Colors.black,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: Container(
+              color: Color(0xffF6747E),
+              child: CupertinoDialogAction(
+                child: const Text(
+                  "삭제",
+                  style: TextStyle(
+                    fontSize: 17,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
