276ff9b11f3fe195a77c671be01867446d8acb6d
yuna <yuna83872@gmail.com>
2024-05-03 00:45:54 +0900

    검색 창 하단 버튼 추가

--- lib/src/pages/closet/p_search.dart
+++ lib/src/pages/closet/p_search.dart
@@ -44,9 +44,61 @@ class SearchClothesState extends State<SearchClothes> {
               const ColorTagPicker(),
               const Divider(color: Color(0xffF0F0F0),),
               const DetailTagPicker(),
+              const SizedBox(height: 16)
             ],
           ),
         ),
+      ),
+      bottomNavigationBar: Container(
+        height: 83,
+        decoration: const BoxDecoration(
+          border: Border(top: BorderSide(color: Colors.black12))
+        ),
+        alignment: Alignment.topCenter,
+        child: Column(
+          children: [
+            const SizedBox(height: 12),
+            Row(
+              children: [
+                const SizedBox(width: 16),
+                Expanded(
+                  flex: 1,
+                  child: SizedBox(
+                    height: 50,
+                    child: OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                        side: const BorderSide(color: Color(0xffCACACA))
+                      ),
+                      onPressed: () {  },
+                      child: const Text(
+                        "초기화",
+                        style: TextStyle(fontSize: 15, color: Colors.black, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                Expanded(
+                  flex: 2,
+                  child: SizedBox(
+                    height: 50,
+                    child: FilledButton(
+                      style: OutlinedButton.styleFrom(
+                          backgroundColor: const Color(0xffF6747E),
+                      ),
+                      onPressed: () {  },
+                      child: const Text(
+                        "검색",
+                        style: TextStyle(fontSize: 17, color: Colors.white, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 16),
+              ],
+            ),
+          ],
+        )
       ),
     );
   }
