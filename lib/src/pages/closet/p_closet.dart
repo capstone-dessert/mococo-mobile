7f7f29a1bc575d77d763f1fdb558897912f50203
dlekgus <97431420+dlekgus@users.noreply.github.com>
2024-05-02 22:14:05 +0900

    여백채우기

--- lib/src/pages/closet/p_closet.dart
+++ lib/src/pages/closet/p_closet.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
+import 'package:mococo_mobile/src/pages/p_search.dart';
 import '../../widgets/app_bar.dart';
 import '../../widgets/image_list.dart';
 import 'p_regist_cloth.dart';
@@ -13,12 +13,62 @@ class Closet extends StatelessWidget {
     return Scaffold(
       appBar: LeftLogoAppBar(onAddButtonPressed: _onAddButtonPressed),
       body: Container(
-          padding: const EdgeInsets.only(left: 16, right: 16),
-          child: const GridviewPage()),
+        padding: const EdgeInsets.only(left: 10, right: 10), //원래 16,16
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              top: 5,
+              left: 6,
+              child: Row(
+                children: <Widget>[
+                  OutlinedButton(
+                    onPressed: () {},
+                    child: Text('전체'),
+                  ),
+                  SizedBox(width: 8),
+                  OutlinedButton(
+                    onPressed: () {
+                      _onSearchButtonPressed();
+                    },
+                    child: Text('검색'),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 55, left: 9),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('0개'),
+                ],
+              ),
+            ),
+            Positioned(
+                top: 40,
+                right: 0,
+                child: TextButton(
+                  child: Text(
+                    '선택',
+                    style: TextStyle(color: Colors.black87),
+                  ),
+                  onPressed: () {},
+                )),
+            Padding(
+              padding: EdgeInsets.only(top: 90, right: 6, left: 6),
+              child: const GridviewPage(),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
   void _onAddButtonPressed() {
     Get.to(AddPage());
+  }
+
+  void _onSearchButtonPressed() {
+    Get.to(SearchPage());
   }
 }
--- lib/src/pages/p_search.dart
+++ lib/src/pages/p_search.dart
@@ -1,4 +1,19 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import '../widgets/app_bar.dart';
-import '../widgets/bottom_navigation_bar.dart';
+import 'package:mococo_mobile/src/widgets/app_bar.dart';
+import 'package:mococo_mobile/src/widgets/tag_list.dart';
+import '../controller/app_bar.dart';
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: const TextTitleAppBar(
+          title: '검색',
+          buttonNum: 0,
+        ),
+        body: SingleChildScrollView());
+  }
+}
