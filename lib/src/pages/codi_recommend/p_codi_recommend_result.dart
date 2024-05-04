7b29d05ad3165e02b8be435ed6949ee6dc420137
yuna <yuna83872@gmail.com>
2024-05-05 01:40:56 +0900

    추천 코드 수정 Bottom Sheet 추가

--- lib/src/pages/codi_recommend/p_codi_recommend_result.dart
+++ lib/src/pages/codi_recommend/p_codi_recommend_result.dart
@@ -1,8 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
 import 'package:mococo_mobile/src/widgets/app_bar.dart';
 
 import '../../components/image_data.dart';
+import '../closet/p_search.dart';
 
 class CodiRecommendResult extends StatefulWidget {
   const CodiRecommendResult({super.key});
@@ -102,7 +105,7 @@ class _CodiRecommendResultState extends State<CodiRecommendResult> {
                     Padding(
                       padding: const EdgeInsets.only(right: 8),
                       child: GestureDetector(
-                        onTap: () {_showModalBottomSheet();},
+                        onTap: () {_showModalBottomSheet(["상의"]);}, // 카테고리 ... 나중에..
                         child: Image.asset(IconPath.topSample,),
                       ),
                     ),
@@ -131,20 +134,22 @@ class _CodiRecommendResultState extends State<CodiRecommendResult> {
     );
   }
 
-  void _showModalBottomSheet() {
+  void _showModalBottomSheet(queries) {
+    int itemCount = 9; // 아이템 개수 나중에 수정
     showModalBottomSheet(
         context: context,
         isScrollControlled: true,
         enableDrag: true,
         elevation: 0,
+        useRootNavigator: true,
         constraints: BoxConstraints(
-          maxHeight: MediaQuery.of(context).size.height - 120 - 83,
-          minHeight: 250,
+          // maxHeight: MediaQuery.of(context).size.height - 120,
+          maxHeight: MediaQuery.of(context).size.height - 600,
+          minHeight: MediaQuery.of(context).size.height - 600,
           minWidth: MediaQuery.of(context).size.width
         ),
         builder: (context) {
           return Container(
-            // height: MediaQuery.of(context).size.height,
             decoration: const BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadius.only(
@@ -156,6 +161,67 @@ class _CodiRecommendResultState extends State<CodiRecommendResult> {
               padding: const EdgeInsets.all(16),
               child: Column(
                 children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children:
+                          List.generate(
+                            queries.length,
+                              (index) {
+                            return Padding(
+                              padding: const EdgeInsets.only(right: 8),
+                              child: Chip(
+                                  backgroundColor: const Color(0xffF9F9F9),
+                                  label: Text(queries[index]),
+                                  labelStyle: const TextStyle(fontWeight: FontWeight.w700, color: Colors.black,),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30.0),
+                                    side: const BorderSide(color: Color(0xffCACACA),),
+                                ),
+                              ),
+                            );
+                          }
+                        ) + [
+                          Padding(
+                            padding: const EdgeInsets.only(),
+                            child: GestureDetector(
+                              onTap: () async {
+                                var newQueries =  await Get.to(() => const SearchClothes(), arguments: queries.toSet());
+                                if (newQueries == [] || newQueries == null) {
+                                  queries = ["전체"];
+                                } else {
+                                  queries = newQueries;
+                                }
+                              },
+                              child: Chip(
+                                backgroundColor: const Color(0xffF9F9F9),
+                                labelPadding: const EdgeInsets.symmetric(horizontal: 5),
+                                label: Image.asset(IconPath.searchTag, width: 20,),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30.0),
+                                  side: const BorderSide(color: Color(0xffF6747E),),
+                                ),
+
+                              ),
+                            ),
+                          ),]
+                      ),
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      const SizedBox(height: 4),
+                      Row(
+                        children: [
+                          Text('$itemCount개',),
+                          const Spacer()
+                        ],
+                      ),
+                      // GridviewPage()
+                    ],
+                  )
 
                 ],
               ),
@@ -164,6 +230,36 @@ class _CodiRecommendResultState extends State<CodiRecommendResult> {
         }
     );
   }
+
+  // void _showModalBottomSheet() {
+  //   Get.bottomSheet(
+  //     isScrollControlled: true,
+  //     backgroundColor: Colors.white,
+  //     elevation: 0,
+  //
+  //     shape: const RoundedRectangleBorder(
+  //         borderRadius: BorderRadius.only(
+  //           topLeft: Radius.circular(20),
+  //           topRight: Radius.circular(20),
+  //         )
+  //     ),
+  //     Padding(
+  //       padding: EdgeInsets.all(16),
+  //       child: Container(
+  //         width: MediaQuery.of(context).size.width,
+  //         constraints: BoxConstraints(
+  //           minHeight: 250,
+  //           maxHeight: MediaQuery.of(context).size.height - 150,
+  //         ),
+  //         child: Column(
+  //           children: [
+  //
+  //           ],
+  //         ),
+  //       ),
+  //     )
+  //   );
+  // }
 
   void _onBackButtonPressed() {
     Navigator.pop(context);
