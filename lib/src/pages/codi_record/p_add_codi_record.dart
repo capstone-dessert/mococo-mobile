5ecfbea6409d6cd2f8fb48c1421c9fe0193eb147
kimsojeong <rtw0202@naver.com>
2024-05-17 00:13:14 +0900

    코디 기록 - 옷 클릭 시 코디 창에 띄우고 드래그해서 배치

--- lib/src/pages/codi_record/p_add_codi_record.dart
+++ lib/src/pages/codi_record/p_add_codi_record.dart
@@ -5,8 +5,8 @@ import 'package:mococo_mobile/src/widgets/weather.dart';
 import 'package:mococo_mobile/src/widgets/modal.dart';
 import 'package:mococo_mobile/src/widgets/search_bottom_sheet.dart';
 import 'package:mococo_mobile/src/widgets/tag_pickers.dart';
-
 import '../../components/image_data.dart';
+import 'dart:math';
 
 class AddCodiRecord extends StatefulWidget {
   const AddCodiRecord({Key? key}) : super(key: key);
@@ -17,6 +17,7 @@ class AddCodiRecord extends StatefulWidget {
 
 class _AddCodiRecordState extends State<AddCodiRecord> {
   List<Widget> codiImages = [];
+  List<ImagePosition> imagePositions = [];
   bool isClothesSelected = false; // 단일 선택 상태
   bool isMultiClothesSelected = false; // 다중 선택 상태
   List<int> selectedClothesIndices = [];
@@ -33,7 +34,6 @@ class _AddCodiRecordState extends State<AddCodiRecord> {
       selectedClothesIndices = selectedIndices;
     });
   }
-
 
   void setSelectedScheduleTag(selectedScheduleTag) {
     setState(() {
@@ -69,11 +69,10 @@ class _AddCodiRecordState extends State<AddCodiRecord> {
                   ],
                 ),
                 const SizedBox(height: 6),
-                // ScheduleTagPicker(setSelectedScheduleTag: setSelectedScheduleTag), // 약속 태그 박스 위 위치
                 selectedClothesIndices.isEmpty ?
-                Container(
+                const SizedBox(
                   height: 400,
-                  child: const Center(
+                  child: Center(
                       child: Text(
                         "코디할 옷을 선택하세요",
                         style: TextStyle(color: Color(0xff999999), fontSize: 15, fontWeight: FontWeight.w500),
@@ -81,15 +80,13 @@ class _AddCodiRecordState extends State<AddCodiRecord> {
                   ),
                 ) :
                 Container(
+                  color: Colors.black12,
                   height: 400,
-                  child: GridView.count(
-                    crossAxisCount: selectedClothesIndices.length,
-                    children: selectedClothesIndices.map((index) {
-                      return _buildClothesImage(index);
-                    }).toList(),
+                  child: Stack(
+                    children: _buildPositionedImages(context, MediaQuery.of(context).size.width - 32, MediaQuery.of(context).size.width),
                   ),
                 ),
-              ScheduleTagPicker(setSelectedScheduleTag: setSelectedScheduleTag), // 약속 태그 박스 아래 위치
+                ScheduleTagPicker(setSelectedScheduleTag: setSelectedScheduleTag),
               ],
             ),
           ),
@@ -113,9 +110,55 @@ class _AddCodiRecordState extends State<AddCodiRecord> {
     // TODO: 저장 버튼 처리
   }
 
-  Widget _buildClothesImage(int index) {
-    return Image.asset(
-      IconPath.topSample ?? '',
+  Widget _buildClothesImage(int index, double imageSize) {
+    return GestureDetector(
+      onPanUpdate: (details) {
+        _handleDrag(details, index);
+      },
+      child: Image.asset(
+        IconPath.topSample ?? '',
+        width: imageSize,
+      ),
     );
   }
+
+  List<Widget> _buildPositionedImages(BuildContext context, double containerWidth, double containerHeight) {
+    return selectedClothesIndices.asMap().entries.map((entry) {
+      int index = entry.key;
+      double left;
+      double top;
+      if (index < imagePositions.length) {
+        left = imagePositions[index].left;
+        top = imagePositions[index].top;
+      } else {
+        left = Random().nextDouble() * containerWidth;
+        top = Random().nextDouble() * containerHeight;
+        imagePositions.add(ImagePosition(left, top));
+      }
+      left = left.clamp(-10, containerWidth - 140);
+      top = top.clamp(-35, containerHeight - 150);
+
+      return Positioned(
+        left: left,
+        top: top,
+        child: _buildClothesImage(entry.value, 150),
+      );
+    }).toList();
+  }
+
+  void _handleDrag(DragUpdateDetails details, int index) {
+    setState(() {
+      imagePositions[index] = ImagePosition(
+        imagePositions[index].left + details.delta.dx,
+        imagePositions[index].top + details.delta.dy,
+      );
+    });
+  }
+}
+
+class ImagePosition {
+  double left;
+  double top;
+
+  ImagePosition(this.left, this.top);
 }
