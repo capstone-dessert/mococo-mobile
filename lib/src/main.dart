aedbd561159b8c3c5536e9db877d4fff54030e10
Yuna <yuna83872@gmail.com>
2024-06-04 14:25:07 +0900

    add flutter_dotenv

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mococo_mobile/src/binding/init_binding.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mococo_mobile/src/app.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
