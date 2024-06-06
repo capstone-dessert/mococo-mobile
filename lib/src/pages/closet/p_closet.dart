2181910b7b7c746208efd8e6ea004d24f3dc1740
Yuna <yuna83872@gmail.com>
2024-06-06 23:28:33 +0900

    fix

--- lib/src/pages/closet/p_closet.dart
+++ lib/src/pages/closet/p_closet.dart
@@ -55,108 +55,109 @@ class _ClosetState extends State<Closet> {
         child: isLoading
           ? const Center(child: CircularProgressIndicator(color: Colors.black12))
           : Stack(
-          children: <Widget>[
-            if (!isMultiClothesSelected)
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 4, left: 8),
-                  child: Row(
-                    children: List.generate(queries.length, (index) {
-                      return Padding(
-                        padding: const EdgeInsets.only(right: 8),
-                        child: Chip(
-                          backgroundColor: const Color(0xffF9F9F9),
-                          label: Text(queries[index]),
-                          labelStyle: const TextStyle(
-                            fontWeight: FontWeight.w700,
-                            color: Colors.black,),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30.0),
-                            side: const BorderSide(
-                              color: Color(0xffCACACA)
-                            ),
-                          ),
-                          materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                        ),
-                      );
-                    }) + [
-                      Padding(
-                        padding: const EdgeInsets.only(),
-                        child: OutlinedButton(
-                          onPressed: () async {
-                            var result = await Get.to(() => const SearchClothes());
-                            setState(() {
-                              queries = result['newQueries'];
-                              clothesList = result['clothesList'];
-                            });
-                          },
-                          style: OutlinedButton.styleFrom(
+            children: <Widget>[
+              if (!isMultiClothesSelected)
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 4, left: 8),
+                    child: Row(
+                      children: List.generate(queries.length, (index) {
+                        return Padding(
+                          padding: const EdgeInsets.only(right: 8),
+                          child: Chip(
                             backgroundColor: const Color(0xffF9F9F9),
-                            side: const BorderSide(
-                              color: Color(0xffF6747E),
-                              width: 1.5,
+                            label: Text(queries[index]),
+                            labelStyle: const TextStyle(
+                              fontWeight: FontWeight.w700,
+                              color: Colors.black,),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                              side: const BorderSide(
+                                color: Color(0xffCACACA)
+                              ),
                             ),
-                            minimumSize: Size.zero,
-                            padding: const EdgeInsets.symmetric(vertical: 9, horizontal: 15),
-                            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
                           ),
-                          child: Image.asset(IconPath.searchTag, width: 20,),
+                        );
+                      }) + [
+                        Padding(
+                          padding: const EdgeInsets.only(),
+                          child: OutlinedButton(
+                            onPressed: () async {
+                              var result = await Get.to(() => const SearchClothes());
+                              setState(() {
+                                queries = result['newQueries'];
+                                clothesList = result['clothesList'];
+                              });
+                            },
+                            style: OutlinedButton.styleFrom(
+                              backgroundColor: const Color(0xffF9F9F9),
+                              side: const BorderSide(
+                                color: Color(0xffF6747E),
+                                width: 1.5,
+                              ),
+                              minimumSize: Size.zero,
+                              padding: const EdgeInsets.symmetric(vertical: 9, horizontal: 15),
+                              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                            ),
+                            child: Image.asset(IconPath.searchTag, width: 20,),
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
+              Padding(
+                padding:
+                EdgeInsets.only(
+                    top: isMultiClothesSelected ? 0 : 58, left: 12),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      isMultiClothesSelected
+                          ? '${selectedClothesIndices.length}개'
+                          : '${clothesList.list.length}개',
+                    ),
+                  ],
+                ),
               ),
-            Padding(
-              padding:
-              EdgeInsets.only(
-                  top: isMultiClothesSelected ? 0 : 58, left: 12),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    isMultiClothesSelected
-                        ? '${selectedClothesIndices.length}개'
-                        : '${clothesList.list.length}개',
-                  ),
-                ],
-              ),
-            ),
-            Positioned(
-              top: 40,
-              right: 0,
-              child: isMultiClothesSelected
+              Positioned(
+                top: 40,
+                right: 0,
+                child: isMultiClothesSelected
                   ? const SizedBox()
                   : TextButton(
-                child: const Text(
-                  '선택',
-                  style: TextStyle(color: Colors.black87),
+                    child: const Text(
+                      '선택',
+                      style: TextStyle(color: Colors.black87),
+                    ),
+                    onPressed: () {
+                      _onMultiClothesSelected();
+                    },
                 ),
-                onPressed: () {
-                  _onMultiClothesSelected();
-                },
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
+              Padding(
+                padding: EdgeInsets.only(
                   top: isMultiClothesSelected ? 24 : 90,
                   right: 6,
-                  left: 6),
-              child: ClothesGridView(
-                state: "detail",
-                getClothesList: getClothesList,
-                onClothesDetail: _onClothesDetail,
-                onLeftLogoAppBar: _onLeftLogoAppBar,
-                isClothesSelected: isClothesSelected,
-                onClothesSelected: _onClothesSelected,
-                isMultiClothesSelected: isMultiClothesSelected,
-                onMultiClothesSelected: _onMultiClothesSelected,
-                selectedClothesIndices: selectedClothesIndices,
-                clothesList: clothesList,
+                  left: 6
+                ),
+                child: ClothesGridView(
+                  state: "detail",
+                  getClothesList: getClothesList,
+                  onClothesDetail: _onClothesDetail,
+                  onLeftLogoAppBar: _onLeftLogoAppBar,
+                  isClothesSelected: isClothesSelected,
+                  onClothesSelected: _onClothesSelected,
+                  isMultiClothesSelected: isMultiClothesSelected,
+                  onMultiClothesSelected: _onMultiClothesSelected,
+                  selectedClothesIndices: selectedClothesIndices,
+                  clothesList: clothesList,
+                ),
               ),
-            ),
-          ],
+            ],
         ),
       )
     );
