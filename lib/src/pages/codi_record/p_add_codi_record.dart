15563176e927ce03d7d56228897583dbc45d9c12
Yuna <yuna83872@gmail.com>
2024-06-13 21:01:36 +0900

    remove unused import

--- lib/src/pages/codi_record/p_add_codi_record.dart
+++ lib/src/pages/codi_record/p_add_codi_record.dart
@@ -1,5 +1,3 @@
-import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
 import 'package:mococo_mobile/src/models/clothes_list.dart';
 import 'package:mococo_mobile/src/models/clothes_preview.dart';
